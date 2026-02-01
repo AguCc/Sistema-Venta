239379451f68c4fb064dcf8c3df4b490080b484b
Agustin Cordoba Cejas <ds21tn.cordoba.agustin@gmail.com>
2026-01-31 23:15:55 -0300

    Actualizacion

--- Formularios/frmUsuarios.cs
+++ Formularios/frmUsuarios.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Sistema_Venta.Utilidades;
+using Entidad;
+using CpNegocio;
 
 namespace Sistema_Venta
 {
@@ -60,7 +62,21 @@ namespace Sistema_Venta
             // TODO: esta línea de código carga datos en la tabla 'dataUsuario.USUARIO' Puede moverla o quitarla según sea necesario.
             this.uSUARIOTableAdapter.Fill(this.dataUsuario.USUARIO);
             cbestado.Items.Add(new OpcionCombo() { Valor = 1, Texto = "Activo" });
-            cbestado.Items.Add(new OpcionCombo() { Valor = 0, Texto = "Activo" });
+            cbestado.Items.Add(new OpcionCombo() { Valor = 0, Texto = "No Activo" });
+            cbestado.DisplayMember = "Texto";
+            cbestado.ValueMember = "Valor";
+            cbestado.SelectedIndex = 0;
+
+            List<Rol> listaRol = new Cn_Rol().listar();
+            foreach (Rol item in listaRol)
+            {
+                cbrol.Items.Add(new OpcionCombo() { Valor = item.IdRol, Texto = item.Descripcion });
+                cbestado.DisplayMember = "Texto";
+                cbestado.ValueMember = "Valor";
+                cbestado.SelectedIndex = 0;
+            }
+
+
 
         }
     }
